c6d86fa6214c492933b6e3c9d12afe4816ef773a
gbnam <gbnam453@gmail.com>
2024-12-24 02:59:01 +0900

    HomeScreen.js 늘봄 아이콘, 알림 아이콘 추가

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'; // SafeAreaView import
+import ButtonIcon from '../assets/images/Buttons/Button_Notification.svg'; // SVG 파일 import
 import colors from '../styles/colors'; // 색상 파일 import
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView style={styles.container}> {/* SafeAreaView로 감쌈 */}
+        <SafeAreaView style={styles.container}>
             <View style={styles.shadowBox}>
                 <Image
                     source={require('../assets/images/Icons/Icon_Neulbom.png')} // 아이콘 경로
                     style={styles.icon}
                 />
-                <Image
-                    source={require('../assets/images/Buttons/Button_Notification.svg')} // 버튼 경로
-                    style={styles.buttonIcon}
-                />
+                <ButtonIcon width={24} height={24}/> {/* SVG 파일을 컴포넌트로 사용 */}
             </View>
         </SafeAreaView>
     );
@@ -22,7 +20,7 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, // 화면 꽉 채움, // 배경 색상을 빨간색으로 변경
+        flex: 1, // 화면 꽉 채움
         backgroundColor: colors.gray050,
     },
     shadowBox: {
@@ -30,18 +28,12 @@ const styles = StyleSheet.create({
         alignItems: 'center', // 세로 중앙 정렬
         justifyContent: 'space-between', // 양쪽 끝으로 정렬
         width: '100%', // 화면 너비에 맞춤
-        height: 60, // 세로 길이 60px
+        height: '8%', // 세로 길이 60px
         paddingLeft: 10, // 왼쪽 여백 10px
-        paddingRight: 10, // 오른쪽 여백 추가 (아이폰 안전구역)
+        paddingRight: 20, // 오른쪽 여백 추가 (아이폰 안전구역)
     },
     icon: {
-        width: 70, // 아이콘 너비
-        height: 70, // 아이콘 높이
-        resizeMode: 'contain', // 비율 유지
-    },
-    buttonIcon: {
-        width: 50, // 버튼 너비
-        height: 50, // 버튼 높이
+        width: '15%', // 아이콘 너비
         resizeMode: 'contain', // 비율 유지
     },
 });
